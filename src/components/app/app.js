b8c3af4ad779004c064d8cf1c14cbc08bff6430c
viski203 <vanek654@gmail.com>
2020-05-23 16:26:22 +0300

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainPageEmpty from '../main/mainEmpty';
+import MainPage from '../main/main'
+import PlaceCard from '../place-card/place-card';
+const App = () => {
+    return (<MainPage />)
+};
+export default App;
---
+++ src/components/place-card/proposition.js
@@ -0,0 +1,7 @@
+const Proposition = [
+    'Wood and stone place',
+    'Beautiful &amp; luxurious apartment at great location',
+    'Canal View Prinsengracht',
+    'Nice, cozy, warm big bed apartment',
+];
+export default Proposition;
